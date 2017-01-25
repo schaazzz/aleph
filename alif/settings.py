61a27d37763b19b2238601e4b5d41e16faddebdc
Shahzeb Ihsan <shahzeb@gmail.com>
2017-01-25 12:55:00 +0800

    Added workspace app

--- alif/settings.py
+++ alif/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'alif.apps.AlifConfig',
+    'editor.apps.EditorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
